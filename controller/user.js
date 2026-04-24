a3b7d0b6209284614b9cd74bb266d131d0e14a3a
Md-meraj21 <mdmerajansari975@gmail.com>
2026-04-24 21:39:05 +0530

    added

--- controller/user.js
+++ controller/user.js
@@ -4,23 +4,23 @@ module.exports.renderSignupForm = (req, res) => {
     res.render("users/signup.ejs");
 };
 
-module.exports.signup = async (req, res) => {
+module.exports.signup = async (req, res, next) => {
     try {
         let { username, email, password } = req.body;
         const newUser = new User({ email, username });
         const registeruser = await User.register(newUser, password);
-        console.log(registeruser);
+
         req.login(registeruser, (err) => {
             if (err) {
                 return next(err);
             }
             req.flash("success", "Welcome To wanderlust");
-            res.redirect("/listings");
-        })
+            return res.redirect("/listings");
+        });
 
     } catch (e) {
         req.flash("error", e.message);
-        res.redirect("/signup");
+        return res.redirect("/signup");
     }
 };
 
@@ -35,13 +35,14 @@ module.exports.Login = async(req, res) => {
     res.redirect(redirectUrl);
 };
 
+
 module.exports.Logout = (req, res, next) => {
     req.logout((err) => {
         if (err) {
             next(err);
         }
         req.flash("success", "Logout Completed !");
-        res.redirect("/listings");
+        return res.redirect("/listings");
     })
 
 };
