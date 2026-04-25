4a39059954feb0b0f24f6fe1653852cf9b420fae
Md-meraj21 <mdmerajansari975@gmail.com>
2026-04-25 22:01:33 +0530

    complete

--- Middleware.js
+++ Middleware.js
@@ -1,45 +1,41 @@
 const Listing = require("./models/listing");
 const ExpressErrors = require("./utils/ExpressErrors.js");
-const {  reviewSchema } = require("./schema.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
 const Review = require("./models/review.js");
 
 module.exports.isLogin = (req, res, next) => {
+  if (!req.isAuthenticated()) {
     req.session.redirectUrl = req.originalUrl;
-    if (!req.isAuthenticated()) {
-        req.flash("error", "You are not logged in!");
-        return res.redirect("/listings");
-    }
-    next();
-}
+    req.flash("error", "You are not logged in!");
+    return res.redirect("/login");
+  }
+  next();
+};
 
-// Middleware.js
 module.exports.saveredirectUrl = (req, res, next) => {
-    if (req.session) {
-        // Only save redirect URL if user is trying to access a protected route
-        if (req.originalUrl && !["/login", "/signup", "/logout"].includes(req.originalUrl)) {
-            req.session.redirectUrl = req.originalUrl;
-        }
-    }
-    next();
+  if (req.session.redirectUrl) {
+    res.locals.redirectUrl = req.session.redirectUrl;
+    delete req.session.redirectUrl;
+  }
+  next();
 };
 
 module.exports.isowner = async (req, res, next) => {
-  let {id } = req.params;
-    let listing = await Listing.findById(id);
-    if (!listing.owner.equals(res.locals.currUser._id)) {
-        req.flash("error", "You don't have permission to edit");
-        return res.redirect(`/listings/${id}`);
-    }
-    next();
-}
+  let { id } = req.params;
+  let listing = await Listing.findById(id);
+  if (!listing.owner.equals(res.locals.currUser._id)) {
+    req.flash("error", "You don't have permission to edit");
+    return res.redirect(`/listings/${id}`);
+  }
+  next();
+};
 
 module.exports.validatelisting = (req, res, next) => {
   let { error } = listingSchema.validate(req.body);
   if (error) {
     let errmsg = error.details.map((el) => el.message).join(",");
     throw new ExpressErrors(404, errmsg);
-  }
-  else {
+  } else {
     next();
   }
 };
@@ -49,15 +45,13 @@ module.exports.validateReview = (req, res, next) => {
   if (error) {
     let errmsg = error.details.map((el) => el.message).join(",");
     throw new ExpressErrors(404, errmsg);
-  }
-  else {
+  } else {
     next();
   }
-}
+};
 
 module.exports.isreviewAuthor = async (req, res, next) => {
   let { id, reviewId } = req.params;
-
   let review = await Review.findById(reviewId);
 
   if (!review || !review.author) {
